2c593184e488dc9216906f9c8cb5a6006eb9c321
KillerSAA <136983879+KillerSAA@users.noreply.github.com>
2024-07-22 00:13:24 -0300

    Update Call.h

--- sdk/Call.h
+++ sdk/Call.h
@@ -33,6 +33,38 @@ public:
 		volatile uintptr_t address_ = address;
 		return reinterpret_cast<Ret(*)(C, Args...)>(address_)(_this, args...);
 	}
+
+	/* I removed '__decl', '__thiscall' etc... as it is not necessary and warnings appear when compiling
+	template <typename Ret, typename... Args>
+	static Ret FunctionCdecl(uintptr_t address, Args... args) {
+		volatile uintptr_t address_ = address;
+		return reinterpret_cast<Ret(__cdecl*)(Args...)>(address_)(args...);
+	}
+
+	template <typename Ret, typename... Args>
+	static Ret FunctionFastCall(uintptr_t address, Args... args) {
+		volatile uintptr_t address_ = address;
+		return reinterpret_cast<Ret(__fastcall*)(Args...)>(address_)(args...);
+	}
+
+	template <typename Ret, typename... Args>
+	static Ret FunctionStdCall(uintptr_t address, Args... args) {
+		volatile uintptr_t address_ = address;
+		return reinterpret_cast<Ret(__stdcall*)(Args...)>(address_)(args...);
+	}
+
+	template <typename Ret, typename... Args>
+	static Ret Function(uintptr_t address, Args... args) {
+		uintptr_t address_ = address;
+		return reinterpret_cast<Ret(__cdecl*)(Args...)>(address_)(args...);
+	}
+
+	template <typename Ret, typename C, typename... Args>
+	static Ret Method(uintptr_t address, C _this, Args... args) {
+		volatile uintptr_t address_ = address;
+		return reinterpret_cast<Ret(__thiscall*)(C, Args...)>(address_)(_this, args...);
+	}
+	*/
 };
 
 
