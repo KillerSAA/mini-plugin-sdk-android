cda82a1d46b78cab6f5e599bb0a05c072672a115
KillerSAA <136983879+KillerSAA@users.noreply.github.com>
2024-07-22 23:36:37 -0300

    added CWidget(and others) and some fixes

---
+++ game_sa/CWidget.cpp
@@ -0,0 +1,141 @@
+#include <mod/amlmod.h>
+#include <game_sa/sdk.h>
+#include <sdk/Call.h>
+
+#include "CWidget.h"
+
+CWidget::CWidget(const char* pszSprite, WidgetPosition *WidgetPos, unsigned int nFlags, HIDMapping Mapping) {
+    Call::FunctionFastCall<void, const char*, WidgetPosition*, unsigned int, HIDMapping>(GetSym("j__ZN7CWidgetC2EPKcRK14WidgetPositionj10HIDMapping"), pszSprite, WidgetPos, nFlags, Mapping);
+}
+
+CWidget::~CWidget() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidgetD2Ev"));
+}
+
+void CWidget::SetTexture(CSprite2d *sprite, unsigned char *pszSprite, bool bForce) {
+    Call::FunctionFastCall<void, CSprite2d*, unsigned char*, bool>(GetSym("_ZN7CWidget10SetTextureER9CSprite2dPcb"), sprite, pszSprite, bForce);
+}
+
+void CWidget::ClearTapHistory() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidget15ClearTapHistoryEv"));
+}
+
+void CWidget::Update() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidget6UpdateEv"));
+}
+
+bool CWidget::AndroidBackEnabled() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget18AndroidBackEnabledEv"));
+}
+
+bool CWidget::IsTouched(CVector2D const& pVecOut) {
+    return Call::FunctionFastCall<bool, CVector2D const&>(GetSym("_ZN7CWidget9IsTouchedEP9CVector2D"), pVecOut);
+}
+
+bool CWidget::IsReleased(CVector2D const& pVecOut) {
+    return Call::FunctionFastCall<bool, CVector2D const&>(GetSym("_ZN7CWidget10IsReleasedEP9CVector2D"), pVecOut);
+}
+
+bool CWidget::IsPinchZooming(CVector2D const& pVecOut) {
+    return Call::FunctionFastCall<bool, CVector2D const&>(GetSym("_ZN7CWidget14IsPinchZoomingEP9CVector2D"), pVecOut);
+}
+
+bool CWidget::IsDoubleTapped() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget14IsDoubleTappedEv"));
+}
+
+bool CWidget::IsSwiped() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget8IsSwipedEv"));
+}
+
+bool CWidget::IsSwipedLeft() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget12IsSwipedLeftEv"));
+}
+
+bool CWidget::IsSwipedRight() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget13IsSwipedRightEv"));
+}
+
+bool CWidget::IsSwipedDown() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget12IsSwipedDownEv"));
+}
+
+bool CWidget::IsSwipedUp() {
+    return Call::FunctionFastCall<bool>(GetSym("_ZN7CWidget10IsSwipedUpEv"));
+}
+
+int CWidget::CoordsAreInsideWidget(CVector2D const& vecCoords, bool bUseExtraPaddingScale) {
+    return Call::FunctionFastCall<int, CVector2D const&, bool>(GetSym("_ZN7CWidget21CoordsAreInsideWidgetERK9CVector2Db"), vecCoords, bUseExtraPaddingScale);
+}
+
+void CWidget::OnInitialTouch() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidget14OnInitialTouchEv"));
+}
+
+float CWidget::GetPinchDistance() {
+    return Call::FunctionFastCall<float>(GetSym("_ZN7CWidget16GetPinchDistanceEv"));
+}
+
+int CWidget::GetNumTapsInTime(float fSeconds) {
+    return Call::FunctionFastCall<int, float>(GetSym("_ZN7CWidget16GetNumTapsInTimeEf"), fSeconds);
+}
+
+void CWidget::DrawHelpIcon(const char* pszChars, float x, float y, float fHeight, int nAlpha) {
+    Call::FunctionFastCall<void, const char*, float, float, float, int>(GetSym("_ZN7CWidget12DrawHelpIconEPKcfffi"), pszChars, x, y, fHeight, nAlpha);
+}
+
+void CWidget::DecrementFrameCount() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidget19DecrementFrameCountEv"));
+}
+
+void CWidget::SetScreenRect(CRect const& rect) {
+    Call::FunctionFastCall<void, CRect>(GetSym("_ZN7CWidget13SetScreenRectERK5CRect"), rect);
+}
+
+void CWidget::ManageAlpha() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidget11ManageAlphaEv"));
+}
+
+void CWidget::CreateHoldEffect(const char* pszSprite, bool bSquare, CRGBA color, bool bExplicitRender) {
+    Call::FunctionFastCall<void, const char*, bool, CRGBA, bool>(GetSym("_ZN7CWidget16CreateHoldEffectEPKcb5CRGBAb"), pszSprite, bSquare, color, bExplicitRender);
+}
+
+void CWidget::SetInitialTouchWidget(int nTouchIndex, CWidget *pInitialTouchWidget) {
+    Call::FunctionFastCall<void, int, CWidget*>(GetSym("_ZN7CWidget21SetInitialTouchWidgetEiPS_"), nTouchIndex, pInitialTouchWidget);
+}
+
+void CWidget::SetReleasedWidget(int nTouchIndex, CWidget *pWidget) {
+    Call::FunctionFastCall<void, int, CWidget*>(GetSym("_ZN7CWidget17SetReleasedWidgetEiPS_"), nTouchIndex, pWidget);
+}
+
+void CWidget::SetPinchZoomWidget(CWidget *pWidget) {
+    Call::FunctionFastCall<void, CWidget*>(GetSym("_ZN7CWidget18SetPinchZoomWidgetEPS_"), pWidget);
+}
+
+void CWidget::SetSwipedWidget(int nTouchIndex, CWidget *pWidget) {
+    Call::FunctionFastCall<void, int, CWidget*>(GetSym("_ZN7CWidget15SetSwipedWidgetEiPS_"), nTouchIndex, pWidget);
+}
+
+void CWidget::GetGradientColor(CRGBA const& color, CWidget *widget, float fLevel, float nAlpha) {
+    Call::FunctionFastCall<void, CRGBA const&, CWidget*, float, float>(GetSym("_ZN7CWidget16GetGradientColorEfi"), color, widget, fLevel, nAlpha);
+} 
+
+void CWidget::UpdateTiming() {
+    Call::FunctionFastCall<void>(GetSym("_ZN7CWidget12UpdateTimingEv"));
+}
+
+void CWidget::SetScissor(CRect const& rectScissor) {
+    Call::FunctionFastCall<void, CRect>(GetSym("_ZN7CWidget10SetScissorER5CRect"), rectScissor);
+}
+
+void CWidget::SetTextureStatic(CSprite2d *sprite, unsigned char* pszSprite) {
+    Call::FunctionFastCall<void, CSprite2d*, unsigned char*>(GetSym("_ZN7CWidget16SetTextureStaticER9CSprite2dPc"), sprite, pszSprite);
+}
+
+bool CWidget::IsInsideRect(CVector2D vecTouch, CRect RectBox) {
+    return Call::FunctionFastCall<bool, CVector2D, CRect>(GetSym("_ZN7CWidget12IsInsideRectE9CVector2D5CRect"), vecTouch, RectBox);
+}
+
+bool CWidget::IsInsideCircle(CVector2D vecTouch, CRect RectBox) {
+    return Call::FunctionFastCall<bool, CVector2D, CRect>(GetSym("_ZN7CWidget14IsInsideCircleE9CVector2D5CRect"), vecTouch, RectBox);
+}   
---
+++ game_sa/CWidget.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "CRGBA.h"
+#include "CSprite2d.h"
+#include "CRect.h"
+#include "CHoldEffect.h"
+#include "eHIDMapping.h"
+#include "CVector.h"
+
+struct CWidget
+{
+    int (**_vptr$CWidget)(void);
+    HIDMapping m_HIDMapping;
+    CSprite2d m_Sprite;
+    struct WidgetPosition {
+    float m_fOriginX;
+    float m_fOriginY;
+    float m_fScaleX;
+    float m_fScaleY;
+    };
+    float m_fFadeRate;
+    CRect m_RectScreen;
+    float m_fExtraPaddingScale;
+    CRect m_RectPaddedScreen;
+    float m_fTapHoldTime;
+    bool m_bTaphold;
+    CRGBA m_Color;
+    bool m_bEnabled;
+    bool m_bCachedEnabled;
+    int8_t undefined; // ??
+    float m_fTapHistory[10];
+    int m_nTouchIndex;
+    int m_nFrameCount;
+    unsigned int m_nFlags;
+    float m_fUserData;
+    CHoldEffect *m_pHoldEffect;
+    float m_fHoldEffectPeriod;
+
+    CWidget(const char* pszSprite, WidgetPosition *WidgetPos, unsigned int nFlags, HIDMapping Mapping);
+    ~CWidget();
+
+    void SetTexture(CSprite2d *sprite, unsigned char *pszSprite, bool bForce);
+    void ClearTapHistory();
+    void Update();
+    void Visualize();
+    bool AndroidBackEnabled();
+    //bool GetEnabled(bool bEnabled); just returns a value from the struct, it is not necessary
+    bool IsTouched(CVector2D const& pVecOut);
+    bool IsReleased(CVector2D const& pVecOut);
+    bool IsPinchZooming(CVector2D const& pVecOut);
+    bool IsDoubleTapped();
+    bool IsSwiped();
+    bool IsSwipedLeft();
+    bool IsSwipedRight();
+    bool IsSwipedDown();
+    bool IsSwipedUp();
+    int CoordsAreInsideWidget(CVector2D const& vecCoords, bool bUseExtraPaddingScale);
+    void OnInitialTouch();
+    float GetPinchDistance();
+    int GetNumTapsInTime(float fSeconds); // maximum taps: 9
+    void DrawHelpIcon(const char* pszChars, float x, float y, float fHeight, int nAlpha);
+    void DecrementFrameCount();
+    void SetScreenRect(CRect const& rect);
+    void ManageAlpha();
+    void CreateHoldEffect(const char* pszSprite, bool bSquare, CRGBA color, bool bExplicitRender);
+    
+    // void SetEnabled(bool bEnabled); it just changes a value in the struct, it is not necessary
+    // float GetWidgetValue(); // just returns m_fUserData
+    // void SetWidgetValue(); // just change the m_fUserData
+
+    // empty functions //
+    /* 
+    void DrawTitleSafeArea(); 
+    void Draw(); 
+    void GetWidgetValue2(); 
+    bool IsButton(); 
+    int IsHeldDown(); 
+    int IsRegion(); 
+    void OnPinchZoomStart();
+    void OnReleasedTouch(); 
+    void OnSwipeTouch(); 
+    void Pulsate(); 
+    void SetEquippedItem(); 
+    void SetSliderRange();
+    void SetWidgetValue2();
+    void SetWidgetValue3();
+    void SetWidgetInfo();
+    void SetWidgetInfo2();
+    */
+
+    // statics //
+    static void SetInitialTouchWidget(int nTouchIndex, CWidget *pInitialTouchWidget);
+    static void SetReleasedWidget(int nTouchIndex, CWidget *pWidget);
+    static void SetPinchZoomWidget(CWidget *pWidget);
+    static void SetSwipedWidget(int nTouchIndex, CWidget *pWidget);
+    static void GetGradientColor(CRGBA const& color, CWidget *widget, float fLevel, float nAlpha); // static or not? 
+    static void UpdateTiming();
+    static void SetScissor(CRect const& rectScissor);
+    static void SetTextureStatic(CSprite2d *sprite, unsigned char* pszSprite);
+    static bool IsInsideRect(CVector2D vecTouch, CRect RectBox);
+    static bool IsInsideCircle(CVector2D vecTouch, CRect RectBox);
+
+    //static void GetScreenWidth(); not necessary, use RsGlobal
+
+};
+
