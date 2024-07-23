71b3edbb1d8c5283ceefa5a91369925fc9d1c901
KillerSAA <136983879+KillerSAA@users.noreply.github.com>
2024-07-22 23:38:01 -0300

    small correction

--- sdk/sdk.cpp
+++ sdk/sdk.cpp
@@ -4,11 +4,10 @@
 
 // static structs
 RsGlobalType* RsGlobal = (RsGlobalType*)(GetSym("RsGlobal"));
-
+void* hGame = aml->GetLibHandle("libGTASA.so");
 //sRadarTrace_Type* sRadarTrace = (sRadarTrace_Type*)(GetSym("_ZN6CRadar13ms_RadarTraceE"));
 
 uintptr_t GetSym(const char* fc){
-    void* hGame = aml->GetLibHandle("libGTASA.so");
     return aml->GetSym(hGame, fc);
 }
 
