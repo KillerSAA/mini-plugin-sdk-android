a091f9a80dc0a49aa667046560345ae9dc7092ba
KillerSAA <136983879+KillerSAA@users.noreply.github.com>
2024-07-24 11:54:43 -0300

    Add files via upload

---
+++ game_sa/CTouchInterface.cpp
@@ -0,0 +1,18 @@
+#include <mod/amlmod.h>
+#include <sdk/Call.h>
+#include <game_sa/sdk.h>
+
+#include "CTouchInterface.h"
+
+int &CTouchInterface::m_nMaxTouches = *(int*)GetSym("_ZN15CTouchInterface13m_nMaxTouchesE");
+const char* CTouchInterface::m_pszWidgetPositionNames = (const char*)GetSym("_ZN15CTouchInterface24m_pszWidgetPositionNamesE");
+CWidget* CTouchInterface::m_pWidgets = (CWidget*)GetSym("_ZN15CTouchInterface10m_pWidgetsE");
+WidgetPosition* CTouchInterface::m_WidgetPosition = (WidgetPosition*)GetSym("_ZN15CTouchInterface16m_WidgetPositionE");
+bool &CTouchInterface::m_bPinchZoom = *(bool*)GetSym("_ZN15CTouchInterface12m_bPinchZoomE");
+bool &CTouchInterface::m_bTouchDown = *(bool*)GetSym("_ZN15CTouchInterface12m_bTouchDownE");
+CVector2D* CTouchInterface::m_vecCachedPos = (CVector2D*)GetSym("_ZN15CTouchInterface14m_vecCachedPosE");
+CVector2D* CTouchInterface::m_vecCachedDelta = (CVector2D*)GetSym("_ZN15CTouchInterface16m_vecCachedDeltaE");
+unsigned char* CTouchInterface::m_pszWidgetTextures = (unsigned char*)GetSym("_ZN15CTouchInterface19m_pszWidgetTexturesE");
+
+// functions //
+// ........
---
+++ game_sa/CTouchInterface.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CRect.h"
+#include "CWidget.h"
+#include "CVector.h"
+
+struct CTouchInterface {
+
+    
+
+    static int &m_nMaxTouches;
+    static const char* m_pszWidgetPositionNames; //m_pszWidgetPositionNames[138]
+    static CWidget *m_pWidgets; // [190]
+    static WidgetPosition *m_WidgetPosition; // [138]
+    static bool &m_bPinchZoom;
+    static bool &m_bTouchDown; // [4]
+    static CVector2D *m_vecCachedPos; // [4]
+    static CVector2D *m_vecCachedDelta;  // [4]
+    static unsigned char *m_pszWidgetTextures; // [128]
+
+    // funcs //
+    
+
+    // statics // 
+    static void LoadDefaultConfigFile();
+    static void LoadFromFile();
+    // ....
+};
